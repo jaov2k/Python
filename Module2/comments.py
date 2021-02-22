3fbdd995bf4313bad6d74c9fd53b1b780472534b
Jose Ortiz <jose@jaov2k.com>
2021-02-22 13:54:41 -0800

    initial commit on comments.py

--- Module2/comments.py
+++ Module2/comments.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     number_of_hours = input("Please Eneter the number of hours that you worked: ")
     number_of_hours = int(number_of_hours)
 
-    #assuming 24 hours in a day
-    percentage_of_day = number_of_hours / 24 *100
+    #Assuming 24 hours in a day
+    #TODO: #1 handle situation when hours is negative
+    percentage_of_day = number_of_hours / 24 * 100
 
     print(f"The percentage of the day you worked is {percentage_of_day}%")
