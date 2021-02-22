6e1fb5bee638fed6f3c9702a626c31d62d898582
Jose Ortiz <jose@jaov2k.com>
2021-02-22 11:23:05 -0800

    initial commit

---
+++ Module2/comments.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    #Prompt user for their hours; need to convert from str to int
+    number_of_hours = input("Please Eneter the number of hours that you worked: ")
+    number_of_hours = int(number_of_hours)
+
+    #assuming 24 hours in a day
+    percentage_of_day = number_of_hours / 24 *100
+
+    print(f"The percentage of the day you worked is {percentage_of_day}%")
