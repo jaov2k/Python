f470e987be42e98931593d41b3bff8d9801021e8
Jose Ortiz <jose@jaov2k.com>
2021-02-23 17:45:50 -0800

    init commit

---
+++ CIS012/Module4/numbers.py
@@ -0,0 +1,38 @@
+if __name__ == "__main__":
+
+    # age = 32
+    # print(age)
+    # print(type(age))
+
+    # # type casting
+    # str_age = '32'
+    # age = int(str_age)
+    # print(age)
+    # print(type(age))
+
+    # # typecast specifying base-2 (binary)
+    # age_binary_str = '100000'
+    # age = int(age_binary_str, 2)
+    # print (age)
+    # print(type(age))
+
+    # # typecast specifying base-16 (hex)
+    # age_hex_str = '20'
+    # age = int(age_hex_str,16)
+    # print(age)
+    # print(type(age))
+
+    # Use underscore as separator for large number readability. Commas create tupples.
+    # one_billion = 1_000_000_000
+    # print(one_billion)
+
+    age = 32.3
+    age = '32.3'
+    age = float(age)
+
+    one_fillion = 1_000_000_000.0
+
+    one_trillion = 1e12
+    print(one_trillion)
+
+    print('script complete')
