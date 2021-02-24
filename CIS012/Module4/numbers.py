46f03c1825ef2be8a64ca95d07250071f182e6fc
Jose Ortiz <jose@jaov2k.com>
2021-02-23 18:02:00 -0800

    finished mod4.1

--- CIS012/Module4/numbers.py
+++ CIS012/Module4/numbers.py
@@ -26,13 +26,32 @@ if __name__ == "__main__":
     # one_billion = 1_000_000_000
     # print(one_billion)
 
-    age = 32.3
-    age = '32.3'
-    age = float(age)
+    # age = 32.3
+    # age = '32.3'
+    # age = float(age)
 
-    one_fillion = 1_000_000_000.0
+    # one_fillion = 1_000_000_000.0
 
-    one_trillion = 1e12
-    print(one_trillion)
+    # one_trillion = 1e12
+    # print(one_trillion)
+
+    # really_small_number = 3.14e-12
+    # print(really_small_number)
+
+    # too_small = -2e400
+    # print(too_small)
+
+    # too_big = 2e400
+    # print(too_big)
+
+    # Use watch variable to see imprecision of float; set break point here
+    # float_val = 0.1 + 0.1 + 0.1
+    # print(float_val)
+
+    #Complex numbers; 'j' is sqrt(-1)
+    my_complex_num = 2 + 3j
+    print(my_complex_num.real)
+    print(my_complex_num.imag)
+    print(my_complex_num.conjugate())
 
     print('script complete')
