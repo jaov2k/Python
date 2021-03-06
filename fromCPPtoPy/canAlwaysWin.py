446dc0049d3e837fc563be959c34de78b59ff618
Jose Ortiz <jose@jaov2k.com>
2021-03-05 19:43:19 -0800

    added comments

--- fromCPPtoPy/canAlwaysWin.py
+++ fromCPPtoPy/canAlwaysWin.py
@@ -1,3 +1,7 @@
+# PURPOSE:  Small program to display the Nim game
+#           Its a single pile of pebbles
+#           Players can take 1 to 8 stones per turn.
+
 def canAlwaysWin(n):
     if 0 < n:
         return True if n % 9 else False
