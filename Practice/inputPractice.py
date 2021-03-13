27baab0f3001d3ccfcb571a1116d14cf9dd7cd2e
Jose Ortiz <jose@jaov2k.com>
2021-03-13 00:17:35 -0800

    testing

---
+++ Practice/inputPractice.py
@@ -0,0 +1,10 @@
+# this is a practice file
+# phrase = input("Please enter a phrase: ")
+# nxt = input("Please enter the substring you wish to replace: \n")
+# nxt2 = input("Please enter the substring you wish to replace it with: \n")
+# print('\"Your replaced message is:\"' + phrase.replace(nxt,nxt2))
+words = "these are more words"
+print("\"This is a string, " + words + "\"")
+print('\\t - \this is a tab\n'
+      '\\n - this is for new line\n'
+      '\\" - this is for quotes')
--- Practice/isInstance.py
+++ Practice/isInstance.py
@@ -4,13 +4,19 @@
 
 oneDimList = [1, 2, 3]
 twoDimList = [[1,2,3],[1,2,3],[1,2,3]]
-print('oneDimList = [1, 2, 3]\n' + 
-      'twoDimList = [[1,2,3],[1,2,3],[1,2,3]]')
+threeDimList = [[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]]
+print('oneDimList   = [1, 2, 3]\n' + 
+      'twoDimList   = [[1,2,3],[1,2,3],[1,2,3]]\n' +
+      'threeDimList = [[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]]')
 print ('isinstance(oneDimList,list)')
 print (isinstance(oneDimList,list))
 print('isinstance(oneDimList[0],list)')
 print (isinstance(oneDimList[0],list))
 print('isinstance(twoDimList,list)')
 print (isinstance(twoDimList,list))
-print('isinstance(twoDimList[0],list)')
-print (isinstance(twoDimList[0],list))
+print('isinstance(twoDimList[0][0],list)')
+print (isinstance(twoDimList[0][0],list))
+print('isinstance(threeDimList,list)')
+print (isinstance(threeDimList,list))
+print('isinstance(threeDimList[0][0],list)')
+print (isinstance(threeDimList[0][0],list))
