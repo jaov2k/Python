88dea5366af032079ca4714e45b76019f3ad7f3e
Jose Ortiz <jose@jaov2k.com>
2021-03-26 09:44:22 -0700

    added TODO list

--- CIS112/Assignment6_Ortiz.py
+++ CIS112/Assignment6_Ortiz.py
@@ -6,8 +6,17 @@
 # PURPOSE:  A Python program which will take file name and it's content from the user.
 #           Save all the input from the user to the file that was created.
 #           Once user is done with providing the content, display the file content on the monitor.
+#           Features include using Function passing, and RegEx.
 
+import re
 # PURPOSE:
 # PARAMETERS:
 # RETURN VALUES:
 # FUNCTION SINGATURE:
+
+if __name__ == "__main__":
+    #TODO: create file
+    #TODO: is fileName Proper. Filename, including extension must be A-Z,a-z, or '_'
+    #TODO: input sentence. Must prompt for addition sentences
+    #TODO: output file content
+    #TODO: #replay
