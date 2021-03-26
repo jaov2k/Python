04ec4de484cb00af3f9ccabbe931bd915bc883aa
Jose Ortiz <jose@jaov2k.com>
2021-03-26 09:28:29 -0700

    init commit

---
+++ CIS112/Assignment6_Ortiz.py
@@ -0,0 +1,13 @@
+# Jose Ortiz
+# 03-26-2021
+# CIS112 - Spring 2021
+# Assignment 6
+#
+# PURPOSE:  A Python program which will take file name and it's content from the user.
+#           Save all the input from the user to the file that was created.
+#           Once user is done with providing the content, display the file content on the monitor.
+
+# PURPOSE:
+# PARAMETERS:
+# RETURN VALUES:
+# FUNCTION SINGATURE:
