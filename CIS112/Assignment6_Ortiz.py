b96ea703ea9ebf9bf04324aff973b13f6869f59e
Jose Ortiz <jose@jaov2k.com>
2021-03-26 12:42:11 -0700

    experimenting

--- CIS112/Assignment6_Ortiz.py
+++ CIS112/Assignment6_Ortiz.py
@@ -22,6 +22,17 @@ import re
 # PARAMETERS:
 # RETURN VALUES:
 # FUNCTION SINGATURE:
+def createFile():
+    filename = input("Please enter a filename: ")
+    print("Filename start is proper...") if re.findall("\A[a-z]|[A-Z]|_",filename) else print("improper filename")
+    print("NUMBERS found...") if not re.findall("\d",filename) else print("No numbers allowed...")
+    print("Special characters NOT found...") if not re.search("[+]",filename) else print ("No special characters allowed...")
 
 if __name__ == "__main__":
-    print("Oh hai!")
+    while True:
+        createFile()
+        replay = input("Play again? (y/n): ")
+        if replay.lower() == 'y':
+            continue
+        else:
+            break
