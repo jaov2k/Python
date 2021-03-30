924b92d87426f7bc5570f3853023f303b55db4df
Jose Ortiz <jose@jaov2k.com>
2021-03-30 13:55:13 -0700

    init commit

---
+++ CIS012/Huh_Assignment05_1/ortiz.py
@@ -0,0 +1,29 @@
+def CalPay(hrs, rate):
+    print("Hours entered are: ", hrs)
+    print("Rate enteres is: ", rate)
+
+def getInput(str):
+    while True:
+        if str == "hours":
+            inp = input("Please enter hours: ")
+        elif str == "rate":
+            inp = input("Please enter rate: ")
+        try:
+            inp = float(inp)
+        except ValueError:
+            print ("No string permitted")
+            continue
+        if inp < 0:
+            print ("No negative numbers")
+        else:
+            return inp
+
+if __name__ == "__main__":
+  while True:
+    CalPay(getInput("hours"), getInput("rate"))    
+    replay = input("Would you like to calc again (y/n): ")    
+    if replay.lower() == 'y':
+        continue
+    else:
+        print("Exiting...")
+        break
