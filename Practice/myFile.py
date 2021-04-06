683358a2ce91b3ec44e67a93bd8b72aa5dc60a5a
Jose Ortiz <jose@jaov2k.com>
2021-04-06 14:27:20 -0700

    noodling

--- Practice/myFile.py
+++ Practice/myFile.py
@@ -1,9 +1,4 @@
-def printFunny():
-    #print("Im a caprisun...")
-    x = None
-    x = 0
-    print (x)
-    x = ""
-    print(x)
-
-printFunny()
+hours = 40
+rate = 20
+print("Hours,",hours,", times a rate of,",rate,"equals $",hours * rate).form
+print(f"Hours, {hours}, times a rate of, {rate}, equals ${hours * rate}")
