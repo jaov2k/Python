2b288ec54ee4c452cf4c1b86d495ecba919c6f34
Jose Ortiz <jose@jaov2k.com>
2021-05-05 16:18:54 -0700

    HuhAssignment07

---
+++ Practice/scratchPaper.py
@@ -0,0 +1,42 @@
+#Prof Huh CIS012 Assignment 7
+#************************************** SAMPLE1.TXT **************************************#
+
+#Variables
+vowelList = 'AEIOUaeiou'
+lines = 0
+vowels = 0
+consonants = 0
+numbers = 0
+
+#File IO
+while True:
+    filename = input("enter filename: ")
+    try:
+        myFile = open(filename,"r")
+    except:
+        print("Incorrect filename or file does not exist. Try Again...")
+    else:
+        break
+data = myFile.read()
+paragraphs = data.split("\n")
+
+#Counting
+for sentences in paragraphs:    
+    for chars in sentences:
+        if chars.isalpha():
+            if chars in vowelList:
+                vowels += 1
+            else:
+                consonants += 1
+        elif chars.isdigit():
+            numbers += 1
+    lines += 1
+
+#Printing
+print (f"Total lines: {lines}\n"
+       f"Total vowels: {vowels}\n"
+       f"Total consonants: {consonants}\n"
+       f"Total numbers: {numbers}\n")
+
+#Closing
+myFile.close()
