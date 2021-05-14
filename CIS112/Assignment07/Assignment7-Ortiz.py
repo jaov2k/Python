adaaa3fd4ebaad035f61ec4212fd38cd3254b145
Jose Ortiz <jose@jaov2k.com>
2021-05-14 09:37:41 -0700

    folder rearranged

---
+++ CIS112/Assignment07/Assignment7-Ortiz.py
@@ -0,0 +1,9 @@
+# Jose Ortiz
+# 05-14-2021
+# CIS112 - Spring 2021
+# Assignment 7
+#
+# PURPOSE:  A program which receives a filename from the user,
+#           a range of pages to be extracted, and a filename
+#           to extract the range of pages to. Program provides
+#           validation of pages range.
