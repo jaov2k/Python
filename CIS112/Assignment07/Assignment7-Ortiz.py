7fc5d399a2793f2ab58c1fa797c83e619de17089
Jose Ortiz <jose@jaov2k.com>
2021-05-14 10:38:01 -0700

    final commit

--- CIS112/Assignment07/Assignment7-Ortiz.py
+++ CIS112/Assignment07/Assignment7-Ortiz.py
@@ -7,3 +7,34 @@
 #           a range of pages to be extracted, and a filename
 #           to extract the range of pages to. Program provides
 #           validation of pages range.
+
+# Import necessary modules
+from pathlib import Path
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+# Get input/output filenames from user, and validate range of pages to be extracted
+input_pdf_path = input('Please enter a file name: ')
+input_pdf = PdfFileReader(str(input_pdf_path))
+while True:
+    startPage = int(input('Please enter the beginning page number to extract: '))
+    endPage = int(input('Please enter the ending page number to extract: '))
+    if startPage > endPage:
+        print ('Your beginning and ending page numbers are not correct.')
+        continue
+    elif endPage > input_pdf.getNumPages():
+        print ('Your ending index is out of range, please enter correct ending page.')
+        continue
+    elif startPage == 0 or endPage == 0:
+        print ('"0" page numbers are not correct.')
+        continue
+    break
+output_pdf_path = input('Please enter output file name: ')
+
+# Add the range of pages to the output object
+pdf_writer = PdfFileWriter()
+for page in input_pdf.pages[startPage:endPage]:
+	pdf_writer.addPage(page)
+
+# Write output file to directory
+with Path(output_pdf_path).open(mode="wb") as output_file:
+	pdf_writer.write(output_file)
