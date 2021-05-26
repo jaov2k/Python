d8c6edf0a14d6d8bb2572a25946e1a17c9f4f0ba
Jose Ortiz <jose@jaov2k.com>
2021-05-26 11:04:13 -0700

    updates

--- CIS012/HuhJasonY/Huh_Assignment09/assignment09.py
+++ CIS012/HuhJasonY/Huh_Assignment09/assignment09.py
@@ -1,4 +1,4 @@
-import string 
+import string
 
 while True:
     while True:
@@ -13,18 +13,25 @@ while True:
     data = fhand.read()
     words = data.split()
     d = {}
+    specialChars = {ord('('):None,
+                    ord(')'):None,
+                    ord('-'):None,
+                    ord(','):None,
+                    ord(';'):None,
+                    ord('.'):None}
 
-    for word in words:
-        word = word.translate(str.maketrans('', '', string.punctuation))
-        word = word.lower()
-        word = word.strip()
+    for word in words:        
+        word = word.translate(specialChars)
+        word = word.lower().strip()       
         if word in d:
             d[word] += 1
         else:
             d[word] = 1
 
     for key, value in sorted (d.items()):
-        print(key, ' : ', value)
+        print(key, ':', value)
+
+    print ('\nThere are', len(d),'different words in the file.\n')
 
     again = input('Do you want to try another file? (y or n) ')
     if again == 'y':
